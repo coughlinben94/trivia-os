33940cd19d63ebd031e628e7576649a0db3a5fe1
Ben Coughlin <coughlinben94@gmail.com>
2026-06-27 20:47:02 -0400

    App: remove /join route (taken down)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Host from './views/Host.jsx'
 import Display from './views/Display.jsx'
-import Join from './views/Join.jsx'
 import Scores from './views/Scores.jsx'
 import AmbientAudit from './views/AmbientAudit.jsx'
 
@@ -11,8 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/host" element={<Host />} />
         <Route path="/display" element={<Display />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/scores" element={<Scores />} />
+<Route path="/scores" element={<Scores />} />
         <Route path="/ambient" element={<AmbientAudit />} />
         <Route path="*" element={<Navigate to="/host" replace />} />
       </Routes>
