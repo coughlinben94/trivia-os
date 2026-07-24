857a46a577af03a6d541cc734921a002dad33914
Ben Coughlin <coughlinben94@gmail.com>
2026-07-24 12:46:55 -0400

    test: Task 6 deliberate breakage for ship.sh smoke-test gate verification

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+throw new Error("ship-gate-task6-deliberate-break")
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
