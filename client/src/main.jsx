063173ccd2580d9caf9a2d378827e0406a510775
Ben Coughlin <coughlinben94@gmail.com>
2026-07-24 14:24:14 -0400

    test: deliberate breakage for deploy-watchdog test-before-promote re-verification

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+throw new Error("watchdog-e2e-rerun-test")
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
